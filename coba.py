76426388cd3fa48c56dba6cbf19e876de5075c82
adeds <adeyds@gmail.com>
2020-01-27 00:14:25 +0700

    list comprehension 2

--- coba.py
+++ coba.py
@@ -1,7 +1,5 @@
-list_a = [1, 2, 3, 4]
-list_b = [2, 3, 4, 5]
-common_num = [
-    a for a in list_a
-    for b in list_b
-    if a == b]
-print(common_num)
+list_a = ["Hello", "World", "In", "Python"]
+small_list_a_1 = str(list_a).lower()
+small_list_a_2 = [_.lower() for _ in list_a]
+print(small_list_a_1)
+print(small_list_a_2)
