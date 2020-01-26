5ccbcb96216be3ec48bc6ffd0646ee9536363725
adeds <adeyds@gmail.com>
2020-01-26 23:13:19 +0700

    for while reverse pyramid

--- coba.py
+++ coba.py
@@ -1,13 +1,14 @@
 space = 0
-for i in range(0, 5):
+height = 10
+for i in range(0, height):
     for k in range(0, i):
         print(' ', end='')
         space = k + 1
-    for j in range(0, (5 - i) * 2):
+    for j in range(0, (height - i) * 2):
         print('*', end='')
     print("*")
-    if i == 4:
+    if i == height - 1:
         while space >= 0:
             print(" ", end='')
             space = space - 1
-        print("*")
+        print("*", '')
