9566e15086d5ff43dc9324f3d9df9edf7a33ff12
adeds <adeyds@gmail.com>
2020-01-26 23:10:38 +0700

    for while reverse pyramid

--- coba.py
+++ coba.py
@@ -1,4 +1,13 @@
-count = 5
-while count:
-    print('The count is: {}'.format(count))
-    count = count - 1  # gada ++ yaa?
+space = 0
+for i in range(0, 5):
+    for k in range(0, i):
+        print(' ', end='')
+        space = k + 1
+    for j in range(0, (5 - i) * 2):
+        print('*', end='')
+    print("*")
+    if i == 4:
+        while space >= 0:
+            print(" ", end='')
+            space = space - 1
+        print("*")
