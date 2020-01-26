124f9dc12e2c799cf211a552e4f643dcabb4eb03
adeds <adeyds@gmail.com>
2020-01-27 00:52:40 +0700

    list comprehension 2 with method

--- coba.py
+++ coba.py
@@ -1,4 +1,6 @@
-list_a = ["Hello", "World", "In", "Python"]
+import string
+
+list_a = ["Hello", "World", "In", "Python", "I'm"]
 
 small_list_a = [_.lower() for _ in list_a]
 str_small_list_a = str(list_a).lower()
@@ -6,16 +8,15 @@ str_small_list_a = str(list_a).lower()
 
 def strList_to_list(strList):
     return strList \
+        .strip(string.punctuation) \
         .replace('[', '') \
         .replace(']', '') \
-        .replace(",", '') \
-        .replace("'", '') \
-        .split()
+        .split("', '")
 
 
 small_list_a2 = list(strList_to_list(str_small_list_a))
 
-print(list_a)
-print(small_list_a)
-print(str_small_list_a)
-print(small_list_a2)
+print('list         ',list_a)
+print('smal list    ',small_list_a)
+print('string       ',str_small_list_a)
+print('smal list2   ',small_list_a)
