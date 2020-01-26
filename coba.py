0ee217ac3ba05d112ba915b8bcad9445aff77546
adeds <adeyds@gmail.com>
2020-01-26 22:44:15 +0700

    null safety

--- coba.py
+++ coba.py
@@ -1,16 +1,4 @@
-amount = (input("Enter amount: "))
-try:
-    amount = int(amount)
-except ValueError as a:
-    pass
-    print(a)
-if isinstance(amount, int):
-    if amount < 1000:
-        discount = amount * 0.05
-        print("Discount", discount)
-    else:
-        discount = amount * 0.10
-        print("Discount", discount)
-    print("Net payable:", amount - discount)
-else:
-    print("salah")
+output = None
+nullData = "No data returned"
+msg = output or nullData
+print(msg)
